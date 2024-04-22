bf9ff147eac379f0248a70cf7622d4bbc6ad3797
PHUC NGUYEN <138844322+PhcNguyen@users.noreply.github.com>
2024-04-22 19:02:15 +0700

    1.0.6

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+
+def __version__() -> str:
+     return open('scripts/.version', 'r').read()
+
+setup(
+    name="Pngz",
+    version=__version__(),
+    author='PhcNguyen',
+    author_email='nguyen098xx@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
