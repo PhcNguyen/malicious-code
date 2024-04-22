097696363190ad453b0ea626283ee74348869e3d
PhcNguyen <phcnguyenz@proton.me>
2024-04-22 13:05:06 +0700

    1.0.6

--- build.py
+++ build.py
@@ -1,15 +1,26 @@
+from subprocess import run, DEVNULL
+from lib.system import System
+
+Terminal = System()
+Terminal.Clear()
+Terminal.Init()
+
 try:
     import google.oauth2  # type: ignore
     import googleapiclient.discovery
     import cryptography
-except:
-    from subprocess import run, DEVNULL
-    packages = ['google-auth-httplib2', 
-                'google-auth-oauthlib', 
-                'google-api-python-client',
-                'cryptography']
-    brk = '--break-system-packages'
+except ImportError as e:
+    try:
+        packages = ['google-auth-httplib2', 
+                    'google-auth-oauthlib', 
+                    'google-api-python-client',
+                    'cryptography']
 
-    for package in packages:
-        run(['pip', 'install', package, brk], stdout=DEVNULL, stderr=DEVNULL)
-        print(f'{package} success !')
+        for package in packages:
+            run(['pip', 'install', package, '--break-system-packages'], stdout=DEVNULL, stderr=DEVNULL)
+            Terminal.Console('Build', f'Installed {package} successfully !', 'Pink')
+    except Exception as e:
+        Terminal.Console('Build', f'Error installing packages: {e}', 'Red')
+        Terminal.Exit()
+else:
+    Terminal.Console('Build', 'All required packages are already installed.', 'Green')
