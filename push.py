0f3c6abc3b2efa49a46917899d3c8ea5de7dab2e
PhcNguyen <phcnguyenz@proton.me>
2024-04-22 12:43:28 +0700

    1.0.6

--- push.py
+++ push.py
@@ -4,7 +4,6 @@ from subprocess import run, DEVNULL
 Terminal = System()
 Terminal.Init()
 Terminal.Clear()
-Terminal.Command('exit')
 
 try:
     with open('scripts/.version', 'r') as file:
@@ -14,8 +13,14 @@ except Exception as e:
     Terminal.Command('exit')
 try:
     run(['git', 'add', '.'], stdout=DEVNULL, stderr=DEVNULL)
+    Terminal.Console('GitHub', 'git add .', 'Red')
+
     run(['git', 'commit', '-m', __version__], stdout=DEVNULL, stderr=DEVNULL)
+    Terminal.Console('GitHub', f'git commit -m "{__version__}"', 'Red')
+
     run(['git', 'push', 'origin', 'main'], stdout=DEVNULL, stderr=DEVNULL)
+    Terminal.Console('GitHub', 'git push origin main', 'Red')
 
 except Exception as e:
     Terminal.Console('GitHub', e, 'Red')
+    Terminal.Command('exit')
