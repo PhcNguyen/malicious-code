7eab181b0f3ec1e6f9bbfa463ad984b03fbc0560
PHUC NGUYEN <138844322+PhcNguyen@users.noreply.github.com>
2024-04-22 16:30:57 +0700

    1.0.6

--- lib/cryptography/padding.py
+++ lib/cryptography/padding.py
@@ -1,10 +1,27 @@
-def pad(plaintext):
+def pad(plaintext: bytes) -> bytes:
+    """
+    Pads the input plaintext to a multiple of 16 bytes.
+
+    Args:
+        plaintext (bytes): The original plaintext.
+
+    Returns:
+        bytes: The padded plaintext.
+    """
     padding_len = 16 - (len(plaintext) % 16)
     padding = bytes([padding_len] * padding_len)
     return plaintext + padding
 
+def unpad(plaintext: bytes) -> bytes:
+    """
+    Removes padding from the input plaintext.
 
-def unpad(plaintext):
+    Args:
+        plaintext (bytes): The padded plaintext.
+
+    Returns:
+        bytes: The original plaintext.
+    """
     padding_len = plaintext[-1]
     assert padding_len > 0
     message, padding = plaintext[:-padding_len], plaintext[-padding_len:]
